1f5dcbc40a4b4bdf86472528f8d46c9e7d226d08
khaoula ouifaya <khaoula.ouifaya@centoria.fr>
2025-06-27 18:12:39 +0100

    first commit

---
+++ Hello-World-service/src/main/java/com/Hello_World_service/Hello_World_service/HelloWorldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Hello_World_service.Hello_World_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldServiceApplication.class, args);
+	}
+
+}
---
+++ Hello-World-service/src/test/java/com/Hello_World_service/Hello_World_service/HelloWorldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hello_World_service.Hello_World_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
